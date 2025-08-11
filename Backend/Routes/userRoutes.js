44726746cca86c2c303b0a65f04cf34f6b730690
CHAMILKA MIHIRAJ <120358764+ChamilkaMihiraj2002@users.noreply.github.com>
2025-08-12 00:19:56 +0530

    Update userRoutes.js

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userRoutes = require('../controllers/user.Controller.js');
-const authenticateToken = require('../Middleware/authMiddleware');
 
-router.get('/', authenticateToken ,userRoutes.getAllUsers); 
-router.get('/:id', authenticateToken , userRoutes.getUserById); 
+router.get('/',userRoutes.getAllUsers); 
+router.get('/:name', userRoutes.getUserById); 
 
 module.exports = router;
