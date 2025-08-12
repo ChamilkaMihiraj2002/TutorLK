d58750843c0380cdf8a56b0f3e160e88a936ae7e
CHAMILKA MIHIRAJ <120358764+ChamilkaMihiraj2002@users.noreply.github.com>
2025-08-12 23:17:04 +0530

    Delete userRoutes.js

--- Backend/Routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userRoutes = require('../controllers/user.Controller.js');
-
-router.get('/',userRoutes.getAllUsers); 
-router.get('/:name', userRoutes.getUserById); 
-
-module.exports = router;
