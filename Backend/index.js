650fc41b6c6981e55c3bc6b7825dd0b8688b0ed5
CHAMILKA MIHIRAJ <120358764+ChamilkaMihiraj2002@users.noreply.github.com>
2025-08-12 23:16:50 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
@@ -13,20 +14,26 @@ const MONGO_URI = process.env.DB_URL;
 // Middleware to parse JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cors());
 
 
-const authRoutes = require('./Routes/authRoutes');
-const userRoutes = require('./Routes/userRoutes');
-const userPosts = require('./Routes/postRoutes');
+const authRoutes = require('./Routes/auth.Routes');
+const userRoutes = require('./Routes/user.Routes');
+const userPosts = require('./Routes/post.Routes');
 
 app.get('/', function (req, res) {
   res.send('Hello World from Express!')
 })
 
+// API Routes
+
+// Public routes
 app.use('/api/auth', authRoutes);
+app.use('/api/users/post',userPosts);
+
+// Protected routes
 app.use('/api/users', authenticateToken ,userRoutes);
-app.use('/api/users/post', authenticateToken ,userPosts);
 
 
 mongoose.connect(MONGO_URI,  {
