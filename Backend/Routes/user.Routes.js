3aefaccf0d7c07dbe4785a0da7bdc995b7167b17
CHAMILKA MIHIRAJ <120358764+ChamilkaMihiraj2002@users.noreply.github.com>
2025-08-14 22:45:56 +0530

    Update user.Routes.js

--- Backend/Routes/user.Routes.js
+++ Backend/Routes/user.Routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const userRoutes = require('../controllers/user.Controller.js');
 
 router.get('/',userRoutes.getAllUsers); 
-router.get('/:name', userRoutes.getUserById); 
+router.get('/:name', userRoutes.getUserById);
+router.delete('/:id', userRoutes.deleteUser);
+router.put('/:id', userRoutes.updateUser); 
 
 module.exports = router;
