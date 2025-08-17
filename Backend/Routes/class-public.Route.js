478b7efb2505d85abc512f2e158ed6eae16a3180
CHAMILKA MIHIRAJ <120358764+ChamilkaMihiraj2002@users.noreply.github.com>
2025-08-17 21:31:57 +0530

    Update class-public.Route.js

--- Backend/Routes/class-public.Route.js
+++ Backend/Routes/class-public.Route.js
@@ -5,6 +5,9 @@ const classController = require('../Controllers/classController');
 
 // Routes
 router.post('/', classController.createClass);                  // Create a new class
+router.get('/:userId', classController.getClassesByUser);       // Get classes by user ID
+router.delete('/:userId', classController.deleteClassById);     // Delete a class by ID
+router.put('/:userId', classController.updateClassById);        // Update a class by ID
 router.get('/all', classController.getClasses);                 // Get all classes
 router.get('/user/:userId', classController.getClassesByUser);  // Get classes by user ID
 
