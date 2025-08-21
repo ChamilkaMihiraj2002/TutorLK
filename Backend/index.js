2310b11ddfdc7a76c28abcb42dd87e75e2e6ac86
CHAMILKA MIHIRAJ <120358764+ChamilkaMihiraj2002@users.noreply.github.com>
2025-08-21 23:04:21 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -33,7 +33,7 @@ const authRoutes = require('./Routes/auth.Routes');
 const userRoutes = require('./Routes/user.Routes');
 const userPosts = require('./Routes/post-public.Routes');
 const postPrivateRoutes = require('./Routes/post-private.Routes');
-const classPublicRoutes = require('./Routes/class-public.Route');
+const classPublicRoutes = require('./Routes/class-private.Route');
 
 app.get('/', function (req, res) {
   res.send('Hello World from Express!')
@@ -44,11 +44,12 @@ app.get('/', function (req, res) {
 // Public routes
 app.use('/api/auth', authRoutes);
 app.use('/api/users/post',userPosts);
-app.use('/api/class', classPublicRoutes);
+
 
 // Protected routes
-app.use('/api/users', authenticateToken ,userRoutes);
-app.use('/api/users/post', authenticateToken ,postPrivateRoutes);
+app.use('/api/users', authenticateToken, userRoutes);
+app.use('/api/users/post', authenticateToken, postPrivateRoutes);
+app.use('/api/class', authenticateToken, classPublicRoutes);
 
 
 mongoose.connect(MONGO_URI,  {
