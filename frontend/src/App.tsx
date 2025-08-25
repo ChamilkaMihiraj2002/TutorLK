ec3b920cdd12a2518e921822496fbf7bc3732d88
CHAMILKA MIHIRAJ <120358764+ChamilkaMihiraj2002@users.noreply.github.com>
2025-08-25 23:43:07 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,27 @@
-import { useState } from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import AppNavbar from './Components/Navbar/Navbar'
+import Home from "./Pages/Home page/Home";
+
+const About: React.FC = () => <h2 className="m-4">About Page</h2>;
+const Services: React.FC = () => <h2 className="m-4">Services Page</h2>;
+const Contact: React.FC = () => <h2 className="m-4">Contact Us Page</h2>;
+const Login: React.FC = () => <h2 className="m-4">Login Page</h2>;
+const SignUp: React.FC = () => <h2 className="m-4">Sign Up Page</h2>;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Hello World</h1>
+      <AppNavbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+
     </>
   )
 }
